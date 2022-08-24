d2fdf2640dd6c18b0f80aa0c09d42d69bec89ebc
Iván <ivan.olea@gmail.com>
2022-08-24 16:35:24 +0200

    Testing chart.js

---
+++ js/loadGpx.js
@@ -0,0 +1,107 @@
+const labels = [
+  ];
+
+  const data = {
+    
+    labels: labels,
+    datasets: [{
+        label: 'Elevación',
+        borderColor: 'rgb(255, 99, 132)',
+        tension: 0.4,
+        fill:false,
+        pointStyle:'circle',
+        pointRadius:0,
+        pointHoverRadius: 15,
+        data: [],
+    }
+    ]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {
+        animation:{
+            onComplete: (animation)=>{
+                //console.log("Grafica renderizada");
+            }
+        },
+        onClick: (e)=>{
+            const canvasPosition = Chart.helpers.getRelativePosition(e,myChart);
+            const dataX = myChart.scales.x.getValueForPixel(canvasPosition.x)/10;
+            const dataY = myChart.scales.y.getValueForPixel(canvasPosition.y);
+            console.log("El indice es: " + myChart.config.data.labels.indexOf(dataX));
+            console.log("El valor es: " + myChart.config.data.datasets[0].data[myChart.config.data.labels.indexOf(dataX)]);
+            console.log("El varlor en labels es: "+myChart.config.data.labels[myChart.config.data.labels.indexOf(dataX)]);
+        }
+    },
+    
+  };
+
+
+function reduceGpx(labels,data,step){
+    newLabelData = labels.map((e)=>{
+        return Math.round(e/step);
+    })
+
+    newIndex=[];
+
+    goodNewData = newLabelData.filter((item,pos)=>{
+        if (newLabelData.indexOf(item)===pos){
+            newIndex.push(pos);
+        }
+        return (newLabelData.indexOf(item)===pos);
+    });
+
+    returnLabels = goodNewData.map((e)=>{
+        return (e*step)/1000;
+    })
+
+    returnData = newIndex.map((e)=>{
+        return data[e];
+    })
+    return{'labels':returnLabels,'data':returnData};
+}
+
+
+
+
+
+
+
+
+
+var gpx = new gpxParser(); //Create gpxParser Object
+var myChart = new Chart(document.getElementById('myChart'),config);
+
+
+
+
+document.querySelector("#file-input").addEventListener('change',(e)=>{
+    let file=e.target.files[0];
+    let reader = new FileReader();
+    reader.addEventListener('load',(e)=>{
+        let text = e.target.result;
+        gpx.parse(text);
+        
+        var newData = reduceGpx(gpx.tracks[0].distance.cumul, gpx.tracks[0].points.map((e)=>{return e.ele}),100);
+        
+
+        
+        myChart.config.data.datasets[0].data=newData.data;
+        myChart.config.data.labels = newData.labels;
+        myChart.options.plugins.title.display=true;
+        myChart.options.plugins.title.text=file.name;
+
+        
+
+
+
+        myChart.update();
+        document.getElementById('myChart').style.display="block";
+        
+    })
+    reader.readAsText(file);
+
+});
+
